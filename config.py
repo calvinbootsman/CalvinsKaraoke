8fbdb4d360ce5e8bb79e364df979265d0f9cb069
Calvin <calvinbootsman@hotmail.com>
2026-04-28 11:04:13 +0200

    moved music to a data directory

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
-MUSIC_DIR = Path("./music")
+MUSIC_DIR = Path("./data/music")
 DEMUX_MODEL = "mdx_extra"
-DEMUX_OUTPUT_ROOT = Path("./separated") / DEMUX_MODEL
+DEMUX_OUTPUT_ROOT = Path("./data/separated") / DEMUX_MODEL
 VALID_AUDIO_EXTENSIONS = {".mp3", ".m4a", ".webm", ".wav", ".flac", ".ogg", ".aac"}
 MAX_DEBUG_EVENTS = 40
 DEBUG_ENABLED = False
